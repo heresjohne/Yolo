8ff5725d4a11dbb6cc2917f18f60011d4ec0fbef
heresjohne <76263096+heresjohne@users.noreply.github.com>
2022-05-09 16:53:03 -0400

    Update decades.py

--- decades.py
+++ decades.py
@@ -8,79 +8,82 @@ import multiprocessing
 import streamlit as st
 #Loading the testing images
 #Loading the saved model
-EVAL_MODEL= './models/model.pth'
-model = torch.load(EVAL_MODEL,map_location ='cpu')
 
-model.eval()
+def decades(opt):
+        EVAL_MODEL= './models/model.pth'
+        model = torch.load(EVAL_MODEL,map_location ='cpu')
 
-
-
-
-bs = 8
-EVAL_DIR='./test/'
+        model.eval()
 
 
-# Prepare the eval data loader
-eval_transform=transforms.Compose([
-        transforms.Resize(size=256),
-        transforms.CenterCrop(size=224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406],
-                             [0.229, 0.224, 0.225])])
-eval_dataset=datasets.ImageFolder(root=EVAL_DIR, transform=eval_transform)
-eval_loader=data.DataLoader(eval_dataset, batch_size=bs, shuffle=True, pin_memory=True)
-# Enable gpu mode, if cuda available
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-# Number of classes and dataset-size
-num_classes=len(eval_dataset.classes)
-dsize=len(eval_dataset)
-# Class label names
-print(eval_dataset.classes)
-#st.header(f'{eval_dataset.classes}')
 
-class_names=['2000-2005','2005-2015','1995-2000','2015-2025','1975-1985','1985-1995']
 
-#class_names=['2000s', '2010s', '90s', '2020s', '70s', '80s']
-# Initialize the prediction and label lists
-predlist=torch.zeros(0,dtype=torch.long, device='cpu')
-lbllist=torch.zeros(0,dtype=torch.long, device='cpu')
-# Evaluate the model accuracy on the dataset
-correct = 0
-total = 0
-with torch.no_grad():
-    for images, labels in eval_loader:
-        images, labels = images.to(device), labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        count = 0
-        for n in predicted: 
-          pred = str(predicted[count])
-          pred = pred.lstrip('tensor()')
-          pred = int(pred.rstrip(')'))
-          pred = class_names[pred]
+        bs = 8
+        EVAL_DIR='./test/'
 
-          lbl = str(labels[count])
-          lbl = lbl.lstrip('tensor()')
-          lbl = int(lbl.rstrip(')'))
 
-          lbl = class_names[lbl]
+        # Prepare the eval data loader
+        eval_transform=transforms.Compose([
+                transforms.Resize(size=256),
+                transforms.CenterCrop(size=224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406],
+                                     [0.229, 0.224, 0.225])])
+        eval_dataset=datasets.ImageFolder(root=EVAL_DIR, transform=eval_transform)
+        eval_loader=data.DataLoader(eval_dataset, batch_size=bs, shuffle=True, pin_memory=True)
+        eval_loader = opt
+        # Enable gpu mode, if cuda available
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        # Number of classes and dataset-size
+        num_classes=len(eval_dataset.classes)
+        dsize=len(eval_dataset)
+        # Class label names
+        print(eval_dataset.classes)
+        #st.header(f'{eval_dataset.classes}')
 
-          print('Predicted Decade is:', pred)
-          print('Actual Decade is',lbl)
-          count +=1 
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-        predlist=torch.cat([predlist,predicted.view(-1).cpu()])
-        lbllist=torch.cat([lbllist,labels.view(-1).cpu()])
-# Overall accuracy
-print(predlist,lbllist)
-overall_accuracy=100 * correct / total
-st.header(f'{overall_accuracy}')
+        class_names=['2000-2005','2005-2015','1995-2000','2015-2025','1975-1985','1985-1995']
 
-print('Accuracy of the network on the {:d} test images: {:.2f}%'.format(dsize, 
-    overall_accuracy))
-# Confusion matrix
-#conf_mat=confusion_matrix(lbllist.numpy(), predlist.numpy())
-#print('Confusion Matrix')
-#print('-'*16)
-#print(conf_mat,'\n')
+        #class_names=['2000s', '2010s', '90s', '2020s', '70s', '80s']
+        # Initialize the prediction and label lists
+        predlist=torch.zeros(0,dtype=torch.long, device='cpu')
+        lbllist=torch.zeros(0,dtype=torch.long, device='cpu')
+        # Evaluate the model accuracy on the dataset
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for images, labels in eval_loader:
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                count = 0
+                for n in predicted: 
+                  pred = str(predicted[count])
+                  pred = pred.lstrip('tensor()')
+                  pred = int(pred.rstrip(')'))
+                  pred = class_names[pred]
+
+                  lbl = str(labels[count])
+                  lbl = lbl.lstrip('tensor()')
+                  lbl = int(lbl.rstrip(')'))
+
+                  lbl = class_names[lbl]
+
+                  print('Predicted Decade is:', pred)
+                  print('Actual Decade is',lbl)
+                  count +=1 
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                predlist=torch.cat([predlist,predicted.view(-1).cpu()])
+                lbllist=torch.cat([lbllist,labels.view(-1).cpu()])
+        # Overall accuracy
+        print(predlist,lbllist)
+        overall_accuracy=100 * correct / total
+        st.header(f'{overall_accuracy}')
+
+        print('Accuracy of the network on the {:d} test images: {:.2f}%'.format(dsize, 
+            overall_accuracy))
+        # Confusion matrix
+        #conf_mat=confusion_matrix(lbllist.numpy(), predlist.numpy())
+        #print('Confusion Matrix')
+        #print('-'*16)
+        #print(conf_mat,'\n')
