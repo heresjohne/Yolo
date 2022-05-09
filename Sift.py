5f337ab1a2c07ef68a1414063ceea8e6c8c69004
heresjohne <76263096+heresjohne@users.noreply.github.com>
2022-05-09 09:55:55 -0400

    Update Sift.py

--- Sift.py
+++ Sift.py
@@ -62,7 +62,7 @@
 import streamlit as st
 import os
 
-def file_selector(folder_path='.'):
+def file_selector(folder_path='./images/'):
     filenames = os.listdir(folder_path)
     selected_filename = st.selectbox('Select a file', filenames)
     return os.path.join(folder_path, selected_filename)
