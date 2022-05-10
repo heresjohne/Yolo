bf88df912b7d989fa8509d72d38afd4937453448
heresjohne <76263096+heresjohne@users.noreply.github.com>
2022-05-09 23:23:09 -0400

    Update detect.py

--- detect.py
+++ detect.py
@@ -128,7 +128,7 @@ def detect(opt, save_img=False):
                         plot_one_box(xyxy, im0, label=label,
                                      color=colors[int(cls)], line_thickness=3)
             if 'person' not in str(s):
-                        b = 'At least 1'
+                        b = n
             else: 
                 b = n
 
