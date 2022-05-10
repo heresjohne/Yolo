e02377daa6583b959f6a89ec388b9b2fa650dc5f
heresjohne <76263096+heresjohne@users.noreply.github.com>
2022-05-09 23:30:54 -0400

    Update detect.py

--- detect.py
+++ detect.py
@@ -128,10 +128,10 @@ def detect(opt, save_img=False):
                         plot_one_box(xyxy, im0, label=label,
                                      color=colors[int(cls)], line_thickness=3)
             if 'person' not in str(s):
-                st.text(s)
-                b = n
+                st.text(f'{s})
+                b = s
             else: 
-                b = n
+                b = s
 
             #LOGGER.info(f'{s} Estimated Band Members: {b}')
             st.button(f'Estimated Band Members: {b}')
